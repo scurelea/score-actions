84bb80fbdd1bd0a7ffaff6fca90da4a7c66170d4
Catalin Demergian <catalin.demergian@enea.com>
2016-05-19 09:32:20 +0300

    Added test asserts
    Signed-off-by: Catalin Demergian <catalin.demergian@enea.com>

--- cs-openstack/src/test/java/io/cloudslang/content/openstack/actions/ListNetworksTest.java
+++ cs-openstack/src/test/java/io/cloudslang/content/openstack/actions/ListNetworksTest.java
@@ -5,18 +5,26 @@ import org.junit.Test;
 
 import java.util.Map;
 
+import static junit.framework.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+
 /**
  * Created by cadm on 5/13/2016.
  */
 public class ListNetworksTest {
 
     ListNetworks listNetworks = new ListNetworks();
+    private final String RETURN_CODE = "returnCode";
+    private final String RETURN_RESULT = "returnResult";
+    private final String STATUS_CODE = "statusCode";
+    private final String AUTH_TOKEN = "authToken";
+    private final String TENANT_ID = "tenantId";
 
 
     @Test
     public void getNetworksListValid() {
 
-        String token = "89324c08215641da876df2d60946c4a4";
+        String token = "3f83caa35c9f426aa7e0292d2e6a15fe";
         String host = "https://stack-9161.hpswlabs.adapps.hp.com";
         String port = "9696";
         String protocol = "";
@@ -39,5 +47,41 @@ public class ListNetworksTest {
         Map<String, String> results = listNetworks.execute(token, host, port, protocol, username, password, proxyHost, proxyPort,
                 proxyUsername, proxyPassword, trustAllRoots, x509HostnameVerifier, trustKeystore, trustPassword,
                 keystore, keystorePassword, connectTimeout, socketTimeout, requestBody);
+
+        assertFalse(results.get(RETURN_RESULT).isEmpty());
+        assertEquals("0", results.get(RETURN_CODE));
+        assertEquals("200", results.get(STATUS_CODE));
+    }
+
+    @Test
+    public void getNetworksListInvalidToken() {
+
+        String token = "abc";
+        String host = "https://stack-9161.hpswlabs.adapps.hp.com";
+        String port = "9696";
+        String protocol = "";
+        String username = "";
+        String password = "";
+        String proxyHost = "";
+        String proxyPort = "";
+        String proxyUsername = "";
+        String proxyPassword = "";
+        String trustAllRoots = "true";
+        String x509HostnameVerifier = "";
+        String trustKeystore = "";
+        String trustPassword = "";
+        String keystore = "";
+        String keystorePassword = "";
+        String connectTimeout = "";
+        String socketTimeout = "";
+        String requestBody = "";
+
+        Map<String, String> results = listNetworks.execute(token, host, port, protocol, username, password, proxyHost, proxyPort,
+                proxyUsername, proxyPassword, trustAllRoots, x509HostnameVerifier, trustKeystore, trustPassword,
+                keystore, keystorePassword, connectTimeout, socketTimeout, requestBody);
+
+        assertFalse(results.get(RETURN_RESULT).isEmpty());
+        assertEquals("-1", results.get(RETURN_CODE));
+        assertEquals("401", results.get(STATUS_CODE));
     }
 }
